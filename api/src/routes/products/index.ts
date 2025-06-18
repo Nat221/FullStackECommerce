b4f0214a54332fcf24458969d2366ee3791d6222
Natko <natko.plevnik@gmail.com>
2025-06-18 12:24:08 +0200

    Validation with zod + drizzle schema

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -6,15 +6,27 @@ import {
   updateProduct,
   deleteProduct,
 } from "./productsController";
+import { validateData } from "../../middlewares/validationMiddleware";
+
+import {
+  createProductSchema,
+  updateProductSchema,
+} from "../../db/productsSchema";
+
+// const createProductSchema = z.object({
+//   name: z.string(),
+//   price: z.number({ message: "Price should be a number" }),
+// });
+
 const router = Router();
 
 router.get("/", listProducts);
 
 router.get("/:id", getProductById);
 
-router.post("/", createProduct);
+router.post("/", validateData(createProductSchema), createProduct);
 
-router.put("/:id", updateProduct);
+router.put("/:id", validateData(updateProductSchema), updateProduct);
 
 router.delete("/:id", deleteProduct);
 
