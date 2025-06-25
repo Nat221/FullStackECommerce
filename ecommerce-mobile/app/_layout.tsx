e423ca4d47a11cd0beb149ef30823afbdb5a8fa7
Natko <natko.plevnik@gmail.com>
2025-06-25 12:44:05 +0200

    install gluestack

--- ecommerce-mobile/app/_layout.tsx
+++ ecommerce-mobile/app/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
 export default function RootDeveloper() {
-  return <Stack />;
+  return (
+    <GluestackUIProvider>
+      <Stack />
+    </GluestackUIProvider>
+  );
 }
