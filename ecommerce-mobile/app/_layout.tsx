627b74ba24f589480981f835a012e17d14388c24
Natko <natko.plevnik@gmail.com>
2025-06-26 15:02:21 +0200

    Fix web and make it responsive

--- ecommerce-mobile/app/_layout.tsx
+++ ecommerce-mobile/app/_layout.tsx
@@ -4,7 +4,10 @@ import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 export default function RootDeveloper() {
   return (
     <GluestackUIProvider>
-      <Stack />
+      <Stack>
+        <Stack.Screen name="index" options={{ title: "Shop" }} />
+        <Stack.Screen name="product/[id]" options={{ title: "Product" }} />
+      </Stack>
     </GluestackUIProvider>
   );
 }
