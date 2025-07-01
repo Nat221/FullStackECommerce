0e984d060f736d6610cd486b54c6418e52dd4a95
Natko <natko.plevnik@gmail.com>
2025-07-01 13:28:05 +0200

    Shopping cart with global state using zustand

--- ecommerce-mobile/app/cart.tsx
+++ ecommerce-mobile/app/cart.tsx
@@ -1,9 +1,46 @@
-import { Text } from "react-native";
+import { Box } from "@/components/ui/box";
+import { HStack } from "@/components/ui/hstack";
+import { VStack } from "@/components/ui/vstack";
+import { useCart } from "@/store/cartStore";
+import { Text } from "@/components/ui/text";
+import { FlatList } from "react-native";
+import { Button, ButtonText } from "@/components/ui/button";
+import { Redirect } from "expo-router";
 
 export default function Cart() {
+  const items = useCart((state) => state.items);
+  const resetCart = useCart((state) => state.resetCart);
+
+  const onCheckout = async () => {
+    //send order to server
+
+    resetCart();
+  };
+
+  if (items.length === 0) {
+    return <Redirect href={"/"} />;
+  }
+
   return (
-    <>
-      <Text>Cart</Text>
-    </>
+    <FlatList
+      data={items}
+      contentContainerClassName="gap-2 max-w-[960px] w-full mx-auto p-2"
+      renderItem={({ item }) => (
+        <Box>
+          <HStack className="p-3">
+            <VStack space="sm">
+              <Text bold>{item.product.name}</Text>
+              <Text>€ {item.product.price}</Text>
+            </VStack>
+            <Text className="ml-auto">{item.quantity}</Text>
+          </HStack>
+        </Box>
+      )}
+      ListFooterComponent={() => (
+        <Button onPress={onCheckout}>
+          <ButtonText>Checkout</ButtonText>
+        </Button>
+      )}
+    />
   );
 }
--- ecommerce-mobile/app/product/[id].tsx
+++ ecommerce-mobile/app/product/[id].tsx
@@ -10,9 +10,13 @@ import { VStack } from "@/components/ui/vstack";
 import { useQuery } from "@tanstack/react-query";
 import { fetchProductById } from "@/api/products";
 import { ActivityIndicator } from "react-native";
+import { useCart } from "@/store/cartStore";
 
 export default function ProductDetailsScreen() {
   const { id } = useLocalSearchParams();
+
+  const addProduct = useCart((state) => state.addProduct);
+  const cartItems = useCart((state) => state.items);
 
   const {
     data: product,
@@ -22,6 +26,10 @@ export default function ProductDetailsScreen() {
     queryKey: ["products", id],
     queryFn: () => fetchProductById(Number(id)),
   });
+
+  const addToCart = () => {
+    addProduct(product);
+  };
 
   if (isLoading) {
     return <ActivityIndicator />;
@@ -53,7 +61,10 @@ export default function ProductDetailsScreen() {
           <Text size="sm">{product.description}</Text>
         </VStack>
         <Box className="flex-col sm:flex-row">
-          <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
+          <Button
+            onPress={addToCart}
+            className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1"
+          >
             <ButtonText size="sm">Add to cart</ButtonText>
           </Button>
           <Button
