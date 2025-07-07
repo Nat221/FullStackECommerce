440bd36cd160ae31d68ad90410094ef0e1c76ccb
Natko <natko.plevnik@gmail.com>
2025-07-07 17:08:49 +0200

    Product list

--- dashboard/next.config.mjs
+++ dashboard/next.config.mjs
@@ -1,7 +1,11 @@
 import { withGluestackUI } from "@gluestack/ui-next-adapter";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["nativewind", "react-native-css-interop"]
+  transpilePackages: [
+    "nativewind",
+    "react-native-css-interop",
+    "@gluestack-ui/image",
+  ],
 };
 
 export default withGluestackUI(nextConfig);
