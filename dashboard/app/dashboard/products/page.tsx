8a410b85e443acd00e9ca347bc2de2c40bb65701
Natko <natko.plevnik@gmail.com>
2025-07-07 17:50:02 +0200

    Product details screen

--- dashboard/app/dashboard/products/page.tsx
+++ dashboard/app/dashboard/products/page.tsx
@@ -3,6 +3,7 @@ import ProductListItem from "./ProductListItem";
 
 export default async function ProductsPage() {
   const products = await listProducts();
+
   return (
     <div className="flex flex-wrap gap-4 max-w-[1400px] mx-auto w-full">
       {products.map((product) => (
